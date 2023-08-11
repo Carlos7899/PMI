44c22f7cc0ac2e9848a5686f16b142848d04c9dd
ViniciusCgama <viniciuscgama@hotmail.com>
2023-08-11 20:23:44 -0300

    Adição note page

--- NotePage.xaml.cs
+++ NotePage.xaml.cs
@@ -6,4 +6,14 @@ public partial class NotePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    public void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        Console.WriteLine(sender.ToString(), e);
+    }
+    public void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
